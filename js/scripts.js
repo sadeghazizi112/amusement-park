8e4378a5dc7d1126fe7124f69931be5cd99dbc30
Sadegh Azizi <sadeghazizi112@yahoo.com>
2018-10-31 11:09:44 -0700

    this is the second version

--- js/scripts.js
+++ js/scripts.js
@@ -3,37 +3,29 @@ var height;
 
 
 $(document).ready(function() {
-  $('#thundermountain').hide();
-  $('#missiontomars').hide();
-  $('#screamer').hide();
-  $('#palerider').hide();
-  $('#mellowyellow').hide();
-  $('#sorry').hide();
   $("form#info").submit(function(event) {
     age = $("input#age").val();
     height = $("input#height").val();
 
 
 
-  $("#result").show();
-
 if (parseInt(age) >= 12 && (height) >= 100 && (height) < 160) {
-  $('#mellowyellow').show();
+  $('#mellowyellow').addClass("canride");
 }
 if ((parseInt(age) >= 15) && (height) >= 110) {
-  $('#missiontomars').show();
+  $('#missiontomars').addClass("canride");
 }
 if (parseInt(age) >= 16 && (height) >= 130) {
-  $('#screamer').show();
+  $('#screamer').addClass("canride");
 }
 if (parseInt(age) >= 18 && (height) >= 160) {
-  $('#thundermountain').show();
+  $('#thundermountain').addClass("canride");
 }
 if ((parseInt(age) >= 20 && (age) < 60) && ((height) >= 170 && (height) < 300)) {
-  $('#palerider').show();
+  $('#palerider').addClass("canride");
 }
 else {
-  $('#sorry').show();
+  $('#sorry').addClass("canride");
 }
     event.preventDefault();
  });
